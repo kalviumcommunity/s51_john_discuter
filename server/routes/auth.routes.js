cf52890c26ec80fa85ef8047be3cbe512e20334e
johncornelles <johncornellews@proton.me>
2024-07-04 13:53:05 +0530

    logout endpoint done

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { login, signUp } from "../controllers/auth.controller.js";
+import { login, logout, signUp } from "../controllers/auth.controller.js";
 
 export const authRouter = Router()
 
 authRouter.post("/login", login)
 authRouter.post("/signup", signUp)
+authRouter.get("/logout", logout)
