fd96f341be58abf290f8596bb363c545bdabbaf4
johncornelles <johncornellews@proton.me>
2024-07-04 15:17:47 +0530

    base setup for checking

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 import axios from "axios"
+import Cookies from "js-cookie"
 
 function App() {
   const [res, setres] = useState("")
   const result = async () => {
-  const res = await axios.get("https://s51-john-discuter.onrender.com/get")
-  setres(res.data)
+  try {
+    const res = await axios.post("https://s51-john-discuter.onrender.com/auth/login", {
+      username: "cornellewsjohn",
+      password: "cornellews"
+    })
+    setres(res.data)
+    console.log(res.data)
+    Cookies.set("jwt", res.data.token)
+  } catch (error) {
+    console.log(error.message)
   }
-  result()
+  }
+  const sub = async () => {
+    try {
+      const jwt = Cookies.get("jwt")
+      const res = await axios.post("https://s51-john-discuter.onrender.com/message/send/667573117b30b114c7eca3ba", {
+        message: "nothing 2",
+        jwt: jwt
+        // jwt: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiI2Njc1MWFhMjU2MGUxOWZjMzhlMzVkOTgiLCJpYXQiOjE3MjAwNjc1NjEsImV4cCI6MTcyMTM2MzU2MX0.nC86NMdoDTedUVdtnpgqVtEekMbLtI3azIlKyGaUEqk"
+      })
+      console.log(res.data)
+      setres(res.data)
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+  useEffect(() => 
+    {result()}
+  , [])
   return (
     <>
+    <h1>hello</h1>
       <h1>{
         JSON.stringify(res)
         }</h1>
+        <button onClick={sub}>
+          send
+        </button>
     </>
   )
 }
