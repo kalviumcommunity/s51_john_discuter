bf847b8df6ba8208ca8e9f0bf4b1bcae56d9f4f1
johncornelles <johncornellews@proton.me>
2024-07-04 15:58:34 +0530

    fixed errors from codium feedback

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,26 +1,26 @@
 import User from "../models/user.model.js";
 
 export const getUsers = async (req, res) => {
-  const userId = req.user._id;
   try {
+    const userId = req.user._id;
     console.log("started in user controller");
-    const users = await User.find({
-      _id: { $ne: userId },
-    }).select("-password");
+    const users = await User.find({ _id: { $ne: userId } }).select("-password");
     res.send(users);
   } catch (error) {
-    console.log("Error: ", error.message, "from user controller");
-    return res.status(500).json("Server Error");
+    console.error("Error: ", error.message, "from user controller");
+    res.status(500).json({
+      message: "Error while getting users: " + error.message
+    });
   }
 };
 
 export const updateUser = async (req, res) => {
-  const userId = req.user._id;
-  const { data } = req.body;
   try {
-    const updatedUser = await User.findByIdAndUpdate(userId, data, {
-      new: true,
-    });
+    const userId = req.user._id;
+    const { data } = req.body;
+
+    const updatedUser = await User.findByIdAndUpdate(userId, data, { new: true });
+
     res.status(200).json({
       _id: updatedUser._id,
       fullName: updatedUser.fullName,
@@ -28,7 +28,9 @@ export const updateUser = async (req, res) => {
       profilePic: updatedUser.profilePic,
     });
   } catch (error) {
-    console.error(error.message); // Use console.error for errors
-    res.status(500).send({ error: "Error updating user" }); // Send error response
+    console.error("Error: ", error.message, "from user controller");
+    res.status(500).send({
+      message: "Error updating user: " + error.message
+    });
   }
 };
